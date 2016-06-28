25f479501d9a33522ab6bdc7d9fc61d699549e7f
Caleb Ariel <ejakaitcaleb@outlook.com>
2016-06-28 10:37:56 +0300

    j
    Init

---
+++ C#/SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using SignalRChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignalRChat.Models;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class UsersContext : DbContext
+        {
+            public UsersContext()
+                : base("DefaultConnection")
+            {
+            }
+
+            public DbSet<UserProfile> UserProfiles { get; set; }
+        }
+
+        [Table("UserProfile")]
+        public class UserProfile
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MydatabaseEntities2 dc=new MydatabaseEntities2())
+                {
+                    //you should check duplicate registration here 
+                    dc.Users.Add(U);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Successfully Registration Done";
+                }
+            }
+            return View(U);
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+
+            if (ModelState.IsValid && Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        private bool Login(string userName, string password, bool persistCookie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
